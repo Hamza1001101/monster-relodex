3eb3e34f9cdc7b9d681e758dfe9dc37a1fa6710f
Hamza Hayd <dado.org@hotmail.com>
2021-06-23 15:47:01 +0200

    changed some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styled from 'styled-components'
 import Card from './components/Card'
 import SearchBox from './components/SearchBox'
