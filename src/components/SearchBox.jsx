6f40b66134bade2a195ba47cf237cc94c206aaf9
Hamza Hayd <dado.org@hotmail.com>
2021-06-27 23:47:51 +0200

    made some style changes

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,14 +2,18 @@ import styled from "styled-components";
 import { device } from "../data";
 
 const Input = styled.input`
-  padding: 1.2rem;
-  width: 20rem;
-  font-size: 1.1rem;
-  cursor: pointer;
-  outline: none;
-  margin: 2rem 0;
-  border-radius: 5px;
-  border: none;
+
+       
+      
+          padding: 1.2rem;
+          width: 20rem;
+          font-size: 1.1rem;
+          cursor: pointer;
+          outline: none;
+          margin: 2rem 0;
+          border-radius: 5px;
+          border: none;
+        
 
   @media ${device.mobileL} {
     width: 15rem;
